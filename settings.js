b10ebe37a2cb68a3c4cec17f31b6915477e26657
muhammadsajidkhanol <b38018060@gmail.com>
2025-06-04 18:39:33 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'PUBG DOLLAR WOW',
   author: '‎',
   botName: "DOLLAR",
-  botOwner: 'NOMI', // Your name
+  botOwner: 'SANDHU', // Your name
   ownerNumber: '12632003961', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
